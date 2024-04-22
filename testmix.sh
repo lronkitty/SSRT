a9eb05df90abc5ce25c93c6a5871162350bcf865
lronkitty <ironkitty@hotmail.com>
2024-04-22 12:42:35 +0800

    main

--- testmix.sh
+++ testmix.sh
@@ -1,4 +1,4 @@
-python main.py gpu_ids=\'0\' data.bs=2 mode=test model=ssrt \
+python main.py gpu_ids=\'4\' data.bs=2 mode=test model=ssrt \
 noise=mixture \
 ckpt=data/checkpoints/mix.ckpt \
 test=icvl_mix
